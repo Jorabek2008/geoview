a87cad0a476da8a8e8658ec0845ba909ca89d8ee
Jo'rabek <jorabekashurqulov400@gmail.com>
2024-11-11 23:18:43 +0500

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./css/index.css";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello adsc world!</h1>
     </div>
   );
 };
